4c1c74a4fdd64bbe1d3de5ba94a605bbdfdfaa8e
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2018-10-28 15:45:14 +0530

    updated version

--- library/index.ts
+++ library/index.ts
@@ -1,5 +1,6 @@
-export { CustomFormControlsModule } from './custom-form-controls/custom-form-controls.module';
-
 export { DynamicFormsModule } from './dynamic-forms/dynamic-forms.module';
 export { ControlBase } from './dynamic-forms/controls/control-base';
 export { ControlsService } from './dynamic-forms/services/controls.service';
+
+
+export { CustomFormControlsModule } from './custom-form-controls/custom-form-controls.module';
